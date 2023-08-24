db3de5b1948bcc9a322748cb57dced55bd0a7a7b
UDAY VARA <udayvara26@gmail.com>
2023-08-24 16:11:48 +0530

    Modifications have been made in the API directory.

--- base/api/urls.py
+++ base/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     
     path('',views.getRoutes), 
+    path('rooms/',views.getRooms),
+    path('rooms/<str:pk>/',views.getRoom)
 ]
--- studybud/settings.py
+++ studybud/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'base.apps.BaseConfig',
+    
+    'rest_framework',
+    
+      "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -129,3 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS : True
