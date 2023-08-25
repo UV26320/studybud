4ff4bda85d14710f79d4ec8f214f5ff693365a86
UDAY VARA <udayvara26@gmail.com>
2023-08-25 17:44:24 +0530

    Apply all required updates.

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from .models import Room,User
+# from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+
+class MyUserCreationForm(UserCreationForm):
+      class Meta:
+          model = User
+          fields = [ 'name', 'username','email', 'password1', 'password2']
+      
 
 class RoomForm(ModelForm):
     class Meta: 
@@ -14,4 +21,5 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta: 
         model = User
-        fields = ['username','email']        
+        fields = ['avatar', 'name' , 'username','email' , 'bio']        
+        
--- base/views.py
+++ base/views.py
@@ -3,11 +3,11 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room,Topic,Message
-from .forms import RoomForm,UserForm
+# from django.contrib.auth.forms import UserCreationForm
+from .models import Room,Topic,Message,User
+from .forms import RoomForm,UserForm,MyUserCreationForm
 
 
 # rooms = [UserCreationForm
@@ -25,13 +25,13 @@ def loginPage(request):
         return redirect('home')
              
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
         
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
             # Authenticate user
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request, email=email, password=password)
             
             if user is not None:
                 login(request, user)
@@ -52,11 +52,11 @@ def logoutUser(request):
 
   
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
       
     if request.method == 'POST':
         
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -227,7 +227,7 @@ def updateUser(request):
     
     if request.method == 'POST':
         
-        form = UserForm(request.POST,instance=user)
+        form = UserForm(request.POST,request.FILES,instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-Profile', pk = user.id)
