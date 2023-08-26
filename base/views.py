1a26737ea6cdfe2c98197c06ddeec194801ba089
UDAY VARA <udayvara26@gmail.com>
2023-08-26 21:24:37 +0530

    Update footer section.

--- base/views.py
+++ base/views.py
@@ -84,7 +84,7 @@ def home(request):
     # room_count =  len(rooms)
     room_count =  rooms.count()
     
-    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))[0:5]
     
     context =  {'rooms': rooms , 'topics': topics , 'room_count' : room_count , 'room_messages': room_messages}
     return render(request, 'base/home.html',context)
