1885372cf700121774cd456cfb625a8fa1e3a4b7
UDAY VARA <udayvara26@gmail.com>
2023-08-31 13:18:38 +0530

    Revised and optimized the vercel.json configuration file.

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*e7@u12a9u1e-ti)$cn^$3djh-iskg0j@(1-j5%mu7ia&4v==@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
